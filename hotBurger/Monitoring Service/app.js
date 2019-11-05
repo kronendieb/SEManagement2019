f8347dc449dc931b9cac76ea9276f81fce26fa63
Cesar Santiago <cas156@students.uwf.edu>
2019-11-05 14:18:16 -0600

    bit of work done

--- hotBurger/Monitoring Service/app.js
+++ hotBurger/Monitoring Service/app.js
@@ -1,24 +1,36 @@
 const express = require("express");
+const app = express();
 const morgan = require("morgan");
 const fs = require("fs");
-//Parse the log file
-const dataFile = fs.readFileSync("./log.json", "utf8");
-const orderStream = fs.JSON.parse(dataFile);
+const json = require("morgan-json");
 
 let logStream = fs.createWriteStream("./log.json", {flags: "a"});
-let app = express();
+const format = json({
+	Method: ":method",
+	Route: ":url",
+	Status: ":status",
+	Response: ":res[content-length]",
+	Response_time: ":response-time ms"
+});
 
+const base = 10;
 app.set("port", 80);
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms', {stream: logStream}));
- 
+app.use(
+	morgan(format, {stream: logStream})
+);
+
+app.use((req, res, next) => {
+	res.send();
+	next();
+});
+
 app.get("/gettotal", function(req, res) {
 	let count = 0;
 	orderStream.forEach(item => {
 		total += item.quantity*item.price;
 		res.send(`${total}`);
 	});
-	
 });
  
 app.get("/gettopseller", function(req, res) {
@@ -34,16 +46,21 @@ app.get("/gettopseller", function(req, res) {
 });
 
 app.get("/getrequestcount", function(req, res) {
-	
 	res.send("");
 });
 
 app.get("/getlastrequeststatus", function(req, res) {
+	
 	res.send("");
 });
 
 app.get("/getlastrequesttime", function(req, res) {
 	res.send("");
+});
+
+app.use((req, res, next) => {
+	res.status(404);
+	res.send("404 - Could not find.");
 });
 
 app.listen(app.get("port"), function() {
--- hotBurger/Ordering Service/app.js
+++ hotBurger/Ordering Service/app.js
@@ -1,26 +1,57 @@
 const express = require("express");
+const app = express();
 const morgan = require("morgan");
 const fs = require("fs");
+const json = require("morgan-json");
 
 let logStream = fs.createWriteStream("./log.json", {flags: "a"});
-let app = express();
+const format = json({
+	Method: ":method",
+	Route: ":url",
+	Status: ":status",
+	Response: ":res[content-length]",
+	Response_time: ":response-time ms"
+});
 
+const base = 10;
 app.set("port", 80);
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms', {stream: logStream}));
+app.use(
+	morgan(format, {stream: logStream})
+);
  
-app.get("/version", function(req, res) {
+app.get("/", (req, res) => {
+	res.send("Welcome to HotBurger.");
+});
+ 
+app.get("/version", (req, res) => {
 	res.send("This is version 1 of the HotBurger service");
 });
  
-app.get("/getmenu", function(req, res) {
-	res.send("Hotdog: $20\nHamburger: $35\nSoda: $4 \nCookie: $6 ");
+app.get("/getmenu", (req, res) => {
+	res.send("Hotdog: $20\nHamburger: $35 \nSoda: $4\nCookie: $6");
 });
 
-//	: variable
-app.post("/purchase/:item/:quantity", function(req, res) {
+app.all("/purchase/:item/:quantity", (req, res) => {
+	
+	let file = fs.readFileSync("./orderType.json", "utf8");
+	orders = JSON.parse(file);
+	
+	orders.forEach(i => {
+		if(i.name === req.params.item){
+			i.quantity += parseInt(req.params.quantity, base);
+		}
+	});
+	
+	fs.writeFileSync("./orderType.json", JSON.stringify(orders));
+	
 	//req.params.item;	gets an attribute of an item
 	res.send(`Thanks for ordering ${req.params.quantity} ${req.params.item} `);
+});
+
+app.use((req, res, next) => {
+	res.status(404);
+	res.send("404 - Could not find.");
 });
  
 app.listen(app.get("port"), function() {
